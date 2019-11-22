a18d4a28a26e0af07b503c0b9e409d57a76d3f2d
Truong Nguyen <truongndph07899@fpt.edu.vn>
2019-11-22 15:23:43 +0700

    them bài

--- app/src/main/java/com/example/duan1_truongndph07899/tacgia.java
+++ app/src/main/java/com/example/duan1_truongndph07899/tacgia.java
@@ -1,4 +1,32 @@
 package com.example.duan1_truongndph07899;
 
-class tacgia {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class tacgia extends Activity {
+    ImageView giupdo,menuchinh;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tacgia);
+
+        giupdo=(ImageView)findViewById(R.id.hinhgiupdo);
+        menuchinh=(ImageView)findViewById(R.id.hinhbackmenu);
+
+        menuchinh.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/duan1_truongndph07899/tudien.java
+++ app/src/main/java/com/example/duan1_truongndph07899/tudien.java
@@ -1,4 +1,40 @@
 package com.example.duan1_truongndph07899;
 
-class tudien {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+public class tudien extends Activity {
+
+    ImageView giupdo, menuchinh;
+    Button tracuu;
+    EditText formtracuu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tudien);
+
+
+        tracuu = (Button) findViewById(R.id.buttontracuu);
+        formtracuu = (EditText) findViewById(R.id.formtracuu);
+
+        giupdo = (ImageView) findViewById(R.id.hinhgiupdo);
+        menuchinh = (ImageView) findViewById(R.id.hinhbackmenu);
+        menuchinh.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
 }
