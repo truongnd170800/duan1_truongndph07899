7e2b20e2163f44397e70d8a936b3edb4ec76253a
Truong Nguyen <truongndph07899@fpt.edu.vn>
2019-11-22 14:56:56 +0700

    them bài

--- app/src/main/java/com/example/duan1_truongndph07899/vanpham.java
+++ app/src/main/java/com/example/duan1_truongndph07899/vanpham.java
@@ -1,4 +1,46 @@
 package com.example.duan1_truongndph07899;
 
-class vanpham {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class vanpham extends Activity {
+    ImageView giupdo, menuchinh;
+    TextView maotu, tinhtu, sosanh, gioitu, lientu, thantu, menhde;
+    private DataBaseHelper myDbHelper;
+    private ArrayList<String> arraywork;
+    private ArrayAdapter<String> arrayadapter;
+    private ListView list;
+    private String TAG = "mysqllite";
+    static public int maotugui = 1;
+    static public int code_thaotac_sua = 888;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vanpham);
+
+        giupdo = (ImageView) findViewById(R.id.hinhgiupdo);
+        menuchinh = (ImageView) findViewById(R.id.hinhbackmenu);
+
+
+        menuchinh.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                finish();
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+
+            }
+        });
+    }
 }
